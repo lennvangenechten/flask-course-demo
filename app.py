6f43b6587846c967b495a0579fe6477c350aa73d
lennvangenechten <r0844917@student.thomasmore.be>
2023-02-19 03:38:42 +0100

    auto-migrations

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ from resources.user import blp as UserBlueprint
 from db import db
 import models
 import os
+from dotenv import load_dotenv
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
